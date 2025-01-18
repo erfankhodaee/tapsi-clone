9e4572da6142cd4469308d1becf5a15e9ae8190a
erfankhodaee <erfankhodaee2013@gmail.com>
2025-01-18 15:13:20 +0330

    added phone element ,almost compelete

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,10 @@
 const ham = document.querySelector(".ham");
 const navmenu = document.querySelector(".nav-menu");
+const banner = document.querySelector(".banner");
+
 
 ham.addEventListener("click", () => {
   ham.classList.toggle("active");
   navmenu.classList.toggle("active");
+  banner.classList.toggle("active");
 });
